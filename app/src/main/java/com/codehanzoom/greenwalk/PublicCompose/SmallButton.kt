946e0e629e37637053fa57d6a2b834b839383199
나희수 <gmltn328@gmail.com>
2024-03-17 23:44:48 +0900

    Design: 공용 컴포넌트 SmallButton 퍼블리싱

---
+++ app/src/main/java/com/codehanzoom/greenwalk/PublicCompose/SmallButton.kt
@@ -0,0 +1,53 @@
+package com.codehanzoom.greenwalk.PublicCompose
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalMapOf
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.codehanzoom.greenwalk.ui.theme.GreenWalkTheme
+
+@Composable
+fun SmallButton(title: String) {
+    Button(
+        shape = RoundedCornerShape(10.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color(0xff8CB369)
+        ),
+        modifier = Modifier
+            .size(100.dp, 40.dp)
+            .padding(0.dp),
+        onClick = { /*TODO*/ }) {
+        Text(
+            text = title,
+            fontFamily = FontFamily.Monospace,
+            fontWeight = FontWeight.Bold,
+            fontSize = 14.sp,
+            textAlign = TextAlign.Center,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SmallButtonPreview() {
+    GreenWalkTheme {
+        SmallButton("기부하기")
+    }
+}
