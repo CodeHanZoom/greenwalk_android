f55cf9ac032bfa4af361bcdef4059b69f6eaedf3
나희수 <gmltn328@gmail.com>
2024-03-23 20:49:44 +0900

    Design: GreenWalk Color Set추가 및 변수명 변경

--- app/src/main/java/com/codehanzoom/greenwalk/ui/theme/Color.kt
+++ app/src/main/java/com/codehanzoom/greenwalk/ui/theme/Color.kt
@@ -11,12 +11,13 @@ val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
 // GreenWalk Project Colosrs
-val GW_Green = Color(0xFF8CB369)
-val GW_Red = Color(0xFFBC4B51)
-val GW_DarkGreen = Color(0xFF5B8E7D)
-val GW_Orange = Color(0xFFF4A259)
-val GW_Yellow = Color(0xFFF4A259)
+val GW_Green100 = Color(0xFF8CB369)
+val GW_Red100 = Color(0xFFBC4B51)
+val GW_Green200 = Color(0xFF5B8E7D)
+val GW_Orange100 = Color(0xFFF4A259)
+val GW_Yellow100 = Color(0xFFF4A259)
 
 // Font Color
-val GW_Black = Color(0xFF252525)
-val GW_Gray = Color(0xFFBDBDBD)
+val GW_Black100 = Color(0xFF252525)
+val GW_Gray100 = Color(0xFFBDBDBD)
+val GW_Red200 = Color(0xFFFF0000)
