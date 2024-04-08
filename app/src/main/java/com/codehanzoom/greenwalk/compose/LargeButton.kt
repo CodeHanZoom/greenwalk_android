9e2f261a0408c07cf4ee123f8f984f227ba6cb09
나희수 <gmltn328@gmail.com>
2024-04-08 21:07:56 +0900

    Refactor: 폴더명 변경으로 인한 경로수정

--- app/src/main/java/com/codehanzoom/greenwalk/compose/LargeButton.kt
+++ app/src/main/java/com/codehanzoom/greenwalk/compose/LargeButton.kt
@@ -1,4 +1,4 @@
-package com.codehanzoom.greenwalk.publicCompose
+package com.codehanzoom.greenwalk.compose
 
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
