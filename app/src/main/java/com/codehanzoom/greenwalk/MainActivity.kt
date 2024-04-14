86d2f0ed44886662d5e918a7e8a92b48ed010ea6
나희수 <gmltn328@gmail.com>
2024-04-14 12:24:19 +0900

    Design: 리소스 변경 및 추가

--- app/src/main/java/com/codehanzoom/greenwalk/MainActivity.kt
+++ app/src/main/java/com/codehanzoom/greenwalk/MainActivity.kt
@@ -4,13 +4,10 @@ import com.codehanzoom.greenwalk.api.SharedPreferencesManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.rememberNavController
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.codehanzoom.greenwalk.nav.NavigationGraph
 import com.codehanzoom.greenwalk.ui.theme.GreenWalkTheme
 import com.codehanzoom.greenwalk.utils.CameraPermissionHandler
-import com.codehanzoom.greenwalk.view.HomeScreen
 
 class MainActivity : ComponentActivity() {
     private lateinit var cameraPermissionHandler: CameraPermissionHandler
@@ -27,6 +24,11 @@ class MainActivity : ComponentActivity() {
         // 전역변수로 생성해서 onCreate보다 먼저 prefs를 초기화하는게 좋다
         // 다른곳에 데이터를 먼저 넘겨줄 수 있다
         prefs = SharedPreferencesManager(applicationContext)
+
+        // splash screen 적용
+        // setContent 전에 실행
+        installSplashScreen()
+
         setContent {
             GreenWalkTheme {
                 NavigationGraph()
