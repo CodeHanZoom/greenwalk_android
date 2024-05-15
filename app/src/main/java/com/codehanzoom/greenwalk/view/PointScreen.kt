677ab39882d23407bf4bd30c7e082a11aa3cc487
gmltn328 <gmltn328@gmail.com>
2024-05-15 23:43:07 +0900

    Design: 포인트 적립 페이지 퍼블리싱

---
+++ app/src/main/java/com/codehanzoom/greenwalk/view/PointScreen.kt
@@ -0,0 +1,206 @@
+package com.codehanzoom.greenwalk.view
+
+import PartnersViewModel
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Text
+import androidx.compose.material3.Divider
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.codehanzoom.greenwalk.MainActivity
+import com.codehanzoom.greenwalk.R
+import com.codehanzoom.greenwalk.compose.LargeButton
+import com.codehanzoom.greenwalk.compose.SmallButton
+import com.codehanzoom.greenwalk.compose.TopBar
+import com.codehanzoom.greenwalk.model.PartnersResponseBody
+import com.codehanzoom.greenwalk.model.UserInfoResponseBody
+import com.codehanzoom.greenwalk.ui.theme.GW_Black100
+import com.codehanzoom.greenwalk.ui.theme.GW_Green100
+import com.codehanzoom.greenwalk.ui.theme.GW_Green200
+import com.codehanzoom.greenwalk.ui.theme.GreenWalkTheme
+import com.codehanzoom.greenwalk.ui.theme.inter_bold
+import com.codehanzoom.greenwalk.ui.theme.inter_regular
+import com.codehanzoom.greenwalk.utils.RetrofitClient
+import retrofit2.Call
+import retrofit2.Response
+
+@Composable
+fun PointScreen(navController: NavHostController) {
+    val TAG = "PointScreen"
+
+    val pointState = remember { mutableStateOf(0) }
+    val trashCountState = remember { mutableStateOf(0) }
+
+    // UI
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        TopBar(title = "포인트 적립", navController = navController)
+        Divider()
+        Spacer(modifier = Modifier.height(30.dp))
+
+        Column(
+            modifier = Modifier.padding(horizontal = 30.dp)
+        ) {
+            Text(
+                text = "나희수" + "님,",
+                fontFamily = inter_bold,
+                color = GW_Black100,
+                fontSize = 20.sp
+            )
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "오늘 ",
+                    fontFamily = inter_regular,
+                    color = GW_Black100,
+                    fontSize = 20.sp
+                )
+                Text(
+                    text = "GreenWalk",
+                    fontFamily = inter_bold,
+                    color = GW_Green200,
+                    fontSize = 24.sp
+                )
+                Text(
+                    text = "를 하면서",
+                    fontFamily = inter_regular,
+                    color = GW_Black100,
+                    fontSize = 20.sp
+                )
+            }
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "${trashCountState.value}" + "개",
+                    fontFamily = inter_bold,
+                    color = GW_Green100,
+                    fontSize = 24.sp
+                )
+                Text(
+                    text = "의 쓰레기를 주웠어요.",
+                    fontFamily = inter_regular,
+                    color = GW_Black100,
+                    fontSize = 20.sp
+                )
+            }
+            Spacer(modifier = Modifier.height(30.dp))
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "${pointState.value}" + "포인트",
+                    fontFamily = inter_bold,
+                    color = GW_Green100,
+                    fontSize = 24.sp
+                )
+                Text(
+                    text = "를 적립했어요.",
+                    fontFamily = inter_regular,
+                    color = GW_Black100,
+                    fontSize = 20.sp
+                )
+            }
+            Spacer(modifier = Modifier.height(60.dp))
+            PartnerScreen(PartnersViewModel())
+        }
+    }
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 700.dp)
+    ) {
+        LargeButton(title = "메인으로") {
+        }
+    }
+}
+
+@Composable
+fun PartnerScreen(viewModel: PartnersViewModel) {
+
+    val partnersResponse by viewModel.partnersResponse.observeAsState()
+
+    Column {
+        if (partnersResponse != null) {
+            LazyColumn {
+                items(partnersResponse!!) { partner ->
+                    Row(
+                        modifier = Modifier
+                            .padding(vertical = 5.dp)
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Row{
+                            Image(
+                                painter = painterResource(id = R.drawable.ic_profile_fill),
+                                contentDescription = "기부처 아이콘",
+                                modifier = Modifier.padding(end = 10.dp)
+                            )
+                            Column{
+                                Text(
+                                    text = partner.name,
+                                    fontFamily = inter_bold,
+                                    color = GW_Black100,
+                                    fontSize = 20.sp
+                                )
+                                Text(
+                                    text = "${partner.totalDonationAmount}P",
+                                    fontFamily = inter_bold,
+                                    color = Color.Gray,
+                                    fontSize = 14.sp
+                                )
+                            }
+                        }
+
+                        SmallButton(title = "기부하기")
+                    }
+                }
+            }
+        } else {
+            Text(text = "로딩 중...")
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun PointScreenPreview() {
+    GreenWalkTheme {
+        val navController = rememberNavController()
+        PointScreen(navController = navController)
+    }
+}
