23489888dccdaee8499bd2678ca220a8174e14e3
gmltn328 <gmltn328@gmail.com>
2024-05-18 00:29:37 +0900

    Refactor: 퍼블리싱된 기부처 목록 업데이트

--- app/src/main/java/com/codehanzoom/greenwalk/view/PointScreen.kt
+++ app/src/main/java/com/codehanzoom/greenwalk/view/PointScreen.kt
@@ -1,8 +1,6 @@
 package com.codehanzoom.greenwalk.view
 
-import PartnersViewModel
-import android.util.Log
-import androidx.compose.foundation.Image
+import PartnersListScreen
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,47 +10,30 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.material3.Divider
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import com.codehanzoom.greenwalk.MainActivity
-import com.codehanzoom.greenwalk.R
 import com.codehanzoom.greenwalk.compose.LargeButton
-import com.codehanzoom.greenwalk.compose.SmallButton
 import com.codehanzoom.greenwalk.compose.TopBar
-import com.codehanzoom.greenwalk.model.PartnersResponseBody
-import com.codehanzoom.greenwalk.model.UserInfoResponseBody
 import com.codehanzoom.greenwalk.ui.theme.GW_Black100
 import com.codehanzoom.greenwalk.ui.theme.GW_Green100
 import com.codehanzoom.greenwalk.ui.theme.GW_Green200
 import com.codehanzoom.greenwalk.ui.theme.GreenWalkTheme
 import com.codehanzoom.greenwalk.ui.theme.inter_bold
 import com.codehanzoom.greenwalk.ui.theme.inter_regular
-import com.codehanzoom.greenwalk.utils.RetrofitClient
-import retrofit2.Call
-import retrofit2.Response
+import com.codehanzoom.greenwalk.viewModel.DonationViewModel
 
 @Composable
 fun PointScreen(navController: NavHostController) {
-    val TAG = "PointScreen"
 
     val pointState = remember { mutableStateOf(0) }
     val trashCountState = remember { mutableStateOf(0) }
@@ -133,7 +114,7 @@ fun PointScreen(navController: NavHostController) {
                 )
             }
             Spacer(modifier = Modifier.height(60.dp))
-            PartnerScreen(PartnersViewModel())
+            PartnersListScreen()
         }
     }
     Box(
@@ -143,54 +124,7 @@ fun PointScreen(navController: NavHostController) {
             .padding(top = 700.dp)
     ) {
         LargeButton(title = "메인으로") {
-        }
-    }
-}
-
-@Composable
-fun PartnerScreen(viewModel: PartnersViewModel) {
-
-    val partnersResponse by viewModel.partnersResponse.observeAsState()
-
-    Column {
-        if (partnersResponse != null) {
-            LazyColumn {
-                items(partnersResponse!!) { partner ->
-                    Row(
-                        modifier = Modifier
-                            .padding(vertical = 5.dp)
-                            .fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceBetween,
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Row{
-                            Image(
-                                painter = painterResource(id = R.drawable.ic_profile_fill),
-                                contentDescription = "기부처 아이콘",
-                                modifier = Modifier.padding(end = 10.dp)
-                            )
-                            Column{
-                                Text(
-                                    text = partner.name,
-                                    fontFamily = inter_bold,
-                                    color = GW_Black100,
-                                    fontSize = 20.sp
-                                )
-                                Text(
-                                    text = "${partner.totalDonationAmount}P",
-                                    fontFamily = inter_bold,
-                                    color = Color.Gray,
-                                    fontSize = 14.sp
-                                )
-                            }
-                        }
-
-                        SmallButton(title = "기부하기")
-                    }
-                }
-            }
-        } else {
-            Text(text = "로딩 중...")
+            navController.navigate("HomeScreen")
         }
     }
 }
