757b5d87169e1abcf81b65a535da3ed53ca08dcb
MCYJ <june1012june@gmail.com>
2024-05-18 21:18:58 +0900

    Feat: adjust preview for masking screen

--- app/src/main/java/com/codehanzoom/greenwalk/utils/Camera.kt
+++ app/src/main/java/com/codehanzoom/greenwalk/utils/Camera.kt
@@ -1,3 +1,5 @@
+
+
 //package com.codehanzoom.greenwalk.utils
 //
 //import android.content.ContentValues
@@ -193,9 +195,7 @@
 //    }
 //}
 //
-//private fun resizeBitmap(bitmap: Bitmap, targetWidth: Int, targetHeight: Int): Bitmap {
-//    return Bitmap.createScaledBitmap(bitmap, targetWidth, targetHeight, false)
-//}
+
 //
 //private fun captureImageAndSendToServer(imageCapture: ImageCapture, context: Context, serverUrl: String, step: Int, walking: Double) {
 //    // 저장명 설정
@@ -313,7 +313,9 @@
 //            }, ContextCompat.getMainExecutor(this))
 //        }
 //    }
-//
+//private fun resizeBitmap(bitmap: Bitmap, targetWidth: Int, targetHeight: Int): Bitmap {
+//    return Bitmap.createScaledBitmap(bitmap, targetWidth, targetHeight, false)
+//}
 //@PreviewCompose
 //@Composable
 //fun CameraPreview() {
@@ -323,6 +325,8 @@
 
 import android.content.ContentValues
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Build
 import android.provider.MediaStore
@@ -368,10 +372,30 @@ import coil.compose.rememberImagePainter
 import com.codehanzoom.greenwalk.MainActivity
 import com.codehanzoom.greenwalk.R
 import com.codehanzoom.greenwalk.compose.TopBar
+import com.codehanzoom.greenwalk.model.PloggingResponseBody
+import com.codehanzoom.greenwalk.utils.UploadService
+import com.codehanzoom.greenwalk.viewModel.PloggingInfoViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.OkHttpClient
+import okhttp3.RequestBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.io.ByteArrayOutputStream
+import java.io.FileNotFoundException
+import java.io.IOException
+import java.util.concurrent.TimeUnit
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
+
+private fun resizeBitmap(bitmap: Bitmap, targetWidth: Int, targetHeight: Int): Bitmap {
+    return Bitmap.createScaledBitmap(bitmap, targetWidth, targetHeight, false)
+}
 
 @Composable
 fun CameraPreviewScreen(navController: NavHostController) {
@@ -421,17 +445,24 @@ fun CameraUI(
             .background(Color.Black)
     ) {
         Spacer(modifier = Modifier.height(100.dp))
-        Box(modifier = Modifier.width(screenWidth)) {
-            AndroidView({ previewView }, modifier = Modifier.height(300.dp).fillMaxWidth())
+        Box(modifier = Modifier
+            .width(screenWidth)
+        ) {
             if (isCapturing && capturedImageUri != null) {
                 Image(
                     painter = rememberImagePainter(capturedImageUri),
                     contentDescription = null,
                     modifier = Modifier
-                        .height(300.dp)
+                        .height(500.dp)
                         .fillMaxWidth(),
                     contentScale = ContentScale.Crop
                 )
+            }
+            else {
+                AndroidView({ previewView },
+                    modifier = Modifier
+                        .height(300.dp)
+                        .fillMaxWidth())
             }
         }
 
@@ -520,7 +551,64 @@ private fun sendImageToServer(
     walking: Double,
     accessToken: String
 ) {
-    // Send image to server logic here
+    val mediaTypeTextPlain = "text/plain".toMediaTypeOrNull()
+    val viewModel = PloggingInfoViewModel()
+
+    try {
+        context.contentResolver.openInputStream(imageUri).use { inputStream ->
+            val bitmap = BitmapFactory.decodeStream(inputStream)
+            val resizedBitmap = resizeBitmap(bitmap, 640, 640)
+            val outputStream = ByteArrayOutputStream()
+            resizedBitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+            val imageBytes = outputStream.toByteArray()
+
+            val okHttpClient = OkHttpClient.Builder()
+                .readTimeout(20, TimeUnit.SECONDS)  // 읽기 타임아웃을 30초로 설정
+                .connectTimeout(20, TimeUnit.SECONDS)  // 연결 타임아웃을 30초로 설정
+                .build()
+
+            val retrofit = Retrofit.Builder()
+                .baseUrl(serverUrl)
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+            val service = retrofit.create(UploadService::class.java)
+            val mediaTypePng = "image/png".toMediaTypeOrNull()
+            val requestFile = RequestBody.create(mediaTypePng, imageBytes)
+            val imagePart = MultipartBody.Part.createFormData("image", "image.png", requestFile)
+            val stepBody = RequestBody.create(mediaTypeTextPlain, step.toString())
+            val walkingBody = RequestBody.create(mediaTypeTextPlain, walking.toString())
+
+            val call = service.uploadImage(imagePart, stepBody, walkingBody, "Bearer $accessToken")
+            call.enqueue(object : Callback<PloggingResponseBody> {
+                override fun onResponse(call: Call<PloggingResponseBody>, response: Response<PloggingResponseBody>) {
+                    if (response.isSuccessful) {
+                        val ploggingInfo = response.body()
+                        ploggingInfo?.let {
+                            body ->
+                            viewModel.setImageUrl(ploggingInfo.imageUrl?:"err")
+                            viewModel.setPoint(ploggingInfo.point?:-1)
+                            viewModel.setTrashCount(ploggingInfo.trashCount?:-1)
+                        }
+
+                        println("response: ${response.message()}, trashCount: ${viewModel.getTrashCount()}")
+                        println("Image uploaded successfully!")
+                    } else {
+                        println("Failed to upload image: ${response.code()}, error: ${response.errorBody()?.string()}")
+                    }
+                }
+
+                override fun onFailure(call: Call<PloggingResponseBody>, t: Throwable) {
+                    println("Network error: ${t.message}")
+                }
+            })
+        }
+    } catch (e: FileNotFoundException) {
+        Log.e("CameraXApp", "File not found: $e")
+    } catch (e: IOException) {
+        Log.e("CameraXApp", "Error accessing file: $e")
+    }
 }
 
 private suspend fun Context.getCameraProvider(): ProcessCameraProvider =
