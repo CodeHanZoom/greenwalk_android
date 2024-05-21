548e5863cb9468b7e97172eb664d8a1416d9edd1
gmltn328 <gmltn328@gmail.com>
2024-05-22 00:26:14 +0900

    Feat: 랭킹 기준 변화

--- app/src/main/java/com/codehanzoom/greenwalk/utils/getGrade.kt
+++ app/src/main/java/com/codehanzoom/greenwalk/utils/getGrade.kt
@@ -3,10 +3,10 @@ package com.codehanzoom.greenwalk.utils
 // getGrade
 // totalPoint에 따라 등급을 반환하는 함수
 // 0~299 300~499 500이상 세 구간으로 구성
-fun getGrade(totalPoint: Int): String {
-    if (totalPoint < 300)
+fun getGrade(accumulatedPoint: Int): String {
+    if (accumulatedPoint < 500)
         return "BRONZE"
-    else if (totalPoint < 500)
+    else if (accumulatedPoint < 1000)
         return "SILVER"
     else
         return "GOLD"
